8de7b7238ca6668c5ce2c6058149886a49ed3e06
Corey Ladovsky <coreyskymusic@gmail.com>
2019-06-27 11:29:18 -0400

    connect nodes

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ class Node {
     this.children = [];
   }
 }
+
+class Graph {
+  constructor() {
+    this.includes = {};
+  }
+
+  connect(node1, node2) {
+    node1.children.push(node2)
+    node2.children.push(node1)
+  }
+
+  build(words, wordLength) {
+    let wordsToBuild = words.filter(word => word.length === wordLength)
+  }
+
+}
