db60bd58de5399d4bbe1b5411eb7e31af04cd87c
Corey Ladovsky <coreyskymusic@gmail.com>
2019-06-28 16:23:06 -0400

    words as links

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-// import { words } from "./dict.js"
+import { words } from "./dict.js"
 import WordLadder from './lib/wordLadder.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-  let words = ["cat", "bat", "bar", "car", "cart", "git", "get", "gin", "card", "cord", "bad", "dad", "did", "dig", "hat", "ham"]
-
   let ladder = new WordLadder(words)
-  let submit = document.querySelector("button");
-  submit.addEventListener("click", () => {
+  let submit = document.querySelector("form");
+  submit.addEventListener("submit", (e) => {
+    debugger
+    e.preventDefault();
     let ul = document.querySelector("ul")
-    ul.innerText = null;
+    ul.innerText = "Thinking... Please wait"
     let inputs = document.querySelectorAll("input")
     let path = ladder.findPath(inputs[0].value, inputs[1].value)
+    ul.innerText = null;
     if(path === "Path Not Possible") {
-      ul.innerText(path)
+      ul.innerText = path
     } else {
       path.forEach(word => {
         let li = document.createElement("li");
-        li.innerText = word;
+        let a = document.createElement("a");
+        a.href = "https://www.merriam-webster.com/dictionary/" + word
+        a.target = "_blank"
+        a.innerText = word;
+        li.appendChild(a)
         ul.appendChild(li)
       })
 
     }
   })
 })
-//
-// let arr = ["cat", "bat", "bar", "car", "cart", "git", "get", "gin", "card", "cord", "bad", "dad", "did", "dig", "hat", "ham"]
-// let ladder = new WordLadder(arr)
-// console.log(ladder)
-// console.log(ladder.findPath("car", "bat"))
-// console.log(ladder.findPath("bat", "car"))
-// console.log(ladder.findPath("car", "gin"))
-// console.log(ladder.findPath("cart", "cord"));
-// console.log("Ladder" , ladder)
