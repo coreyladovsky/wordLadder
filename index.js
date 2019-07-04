d3bb985a585f24ff73a8c2df6b87013d7dcea758
Corey Ladovsky <coreyskymusic@gmail.com>
2019-07-04 19:16:42 -0400

    add canvas

--- index.js
+++ index.js
@@ -32,35 +32,87 @@ document.addEventListener("DOMContentLoaded", () => {
             ul.appendChild(li)
           })
 
-          let svg = d3.select('body').append('svg')
-            .attr('width', 800)
-            .attr('height', 800);
+          let canvas = d3.select('#canvas');
+          let width = canvas.attr("width");
+          let height = canvas.attr("height");
+          let radius = 3;
+          let ctx = canvas.node().getContext("2d");
 
-            let simulation = d3.forceSimulation()
-              .force("link",d3.forceLink().id(function (d) {return d.val;}).distance(100).strength(1))
-              .force("charge", d3.forceManyBody())
-              .force("center", d3.forceCenter(width / 2, height / 2));
+          Object.values(ladder.currGraph.graph).forEach(node => {
+            node.x = Math.random() * width;
+            node.y = Math.random() * y;
+          })
 
+          function update() {
+            ctx.clearRect(0, 0, width, height);
+            ctx.beginPath();
+            Object.values(ladder.currGraph.graph).forEach(drawNode);
+            ctx.fill();
+          }
+
+          function drawNode(node) {
+            ctx.moveTo(node.x, node.y);
+            ctx.arc(node.x, node.y, radius, 0, 2 * Math.PI)
+          }
+
+          update();
+
+          // let svg = d3.select('body').append('svg')
+          //   .attr('width', 800)
+          //   .attr('height', 800);
+          //
+          //   let simulation = d3.forceSimulation()
+          //     .force("link", d3.forceLink(ladder.currGraph.links))
+          //     .force("link",d3.forceLink().id(function (d) {return d.val;}).distance(100).strength(1))
+          //     .force("charge", d3.forceManyBody())
+          //     .force("center", d3.forceCenter(800 / 2, 800 / 2));
+          //
+          //     simulation
+          //       .nodes(ladder.currGraph.graph)
+          //       .on("tick", ticked);
+          //
+          //   simulation.force("link")
+          //       .links(ladder.currGraph.links);
+          //
+          //     simulation.nodes()
+          //
+          //
+          //     function ticked() {
+          //         context.clearRect(0, 0, width, height);
+          //
+          //         context.beginPath();
+          //         graph.links.forEach(drawLink);
+          //         context.strokeStyle = "#aaa";
+          //         context.stroke();
+          //
+          //         context.beginPath();
+          //         graph.nodes.forEach(drawNode);
+          //         context.fill();
+          //         context.strokeStyle = "#fff";
+          //         context.stroke();
+          //       }
                         // .nodes(d3.values(ladder.currGraph.graph))
                         // .links(ladder.currGraph.links)
                         // .gravity(.05)
                         // .distance(100)
                         // .charge(-100)
                         // .start()
+                        // console.log(simulation);
 
-            let link = svg.selectAll('.link')
-                        .data(ladder.currGraph.links)
-                        .enter()
-                        .append('line')
-                        .attr('class', 'link')
+            // let link = svg.selectAll('.link')
+            //             .data(ladder.currGraph.links)
+            //             .enter()
+            //             .append('line')
+            //             .attr('class', 'link')
+            // //
+            // let node = svg.selectAll('.node')
+            //             .data(ladder.currGraph.graph)
+            //             .enter()
+            //             .append('circle')
+            //             .attr('class', 'node')
+            //             .attr('r', 800 * 0.03)
 
-            let node = svg.selectAll('.node')
-                        .data(force.nodes())
-                        .enter()
-                        .append('circle')
-                        .attr('class', 'node')
-                        .attr('r', 800 * 0.03)
-          console.log(ladder.currGraph)
+          // console.log(ladder.currGraph)
 
         }
       }, 0)
