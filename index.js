558e090b69d4c109962fc2577621c0e1d8453819
Corey Ladovsky <coreyskymusic@gmail.com>
2019-07-05 22:57:18 -0400

    working on creating visual build

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import WordLadder from './lib/wordLadder.js';
-// import words from 'an-array-of-english-words';
+import words from 'an-array-of-english-words';
+// import VisualBuild from './lib/visualBuild.js';
 import * as d3 from 'd3'
 
 document.addEventListener("DOMContentLoaded", () => {
-  let words = ["cat", "can", "car", "bar", "bat", "bit"]
+  let words = ["cat", "can", "car", "bar", "bat", "bit", "hit", "him", "hid", "did", "dig", "dad", "bad", "big", "gig", "gave", "give"]
   let ladder = new WordLadder(words)
   let submit = document.querySelector("form");
   let ul = document.querySelector("ul")
@@ -27,33 +28,30 @@ document.addEventListener("DOMContentLoaded", () => {
             let a = document.createElement("a");
             a.href = "https://www.merriam-webster.com/dictionary/" + word
             a.target = "_blank"
-            a.innerText = word;
+            a.innerText = word.val;
             li.appendChild(a)
             ul.appendChild(li)
           })
-
+          // let visual = new VisualBuild();
+          // visual.buildStartWordToEndWord(path[0], path[path.length - 1])
           let canvas = d3.select('#canvas');
           let width = canvas.attr("width");
           let height = canvas.attr("height");
           let radius = 3;
-          // let color = d3.scaleOrdinal(d3.schemeCategory20)
+          let color = d3.scaleOrdinal(d3.schemeCategory10)
           let ctx = canvas.node().getContext("2d");
 
           let simulation = d3.forceSimulation()
             .force("x", d3.forceX(width / 2))
             .force("y", d3.forceY(height / 2))
             .force("collide", d3.forceCollide(radius + 1))
-            .force("charge", d3.forceManyBody().strength(-20))
+            .force("charge", d3.forceManyBody().strength(-200))
             .force("link", d3.forceLink().id((d) => d.val))
             .on("tick", update)
 
             simulation.nodes(Object.values(ladder.currGraph.graph))
             simulation.force("link")
                 .links(ladder.currGraph.links)
-          // Object.values(ladder.currGraph.graph).forEach(node => {
-          //   node.x = Math.random() * width;
-          //   node.y = Math.random() * height;
-          // })
 
 
         canvas
@@ -66,16 +64,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
           function update() {
             ctx.clearRect(0, 0, width, height);
+            ctx.globalAlpha = 0.8;
             ctx.beginPath();
-            // ctx.globalAlpha = 0.1;
             ctx.strokeStyle = "#aaa"
             ladder.currGraph.links.forEach(drawLink);
             ctx.stroke();
-            // ctx.beginPath();
             Object.values(ladder.currGraph.graph).forEach(drawNode);
-            // ctx.fill();
-
-
           }
 
           // credit to https://bl.ocks.org/mbostock/ad70335eeef6d167bc36fd3c04378048
@@ -86,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
           function drawNode(node) {
             ctx.beginPath();
 
-            ctx.fillStyle = 'green'
+            ctx.fillStyle = color(node.weight)
             ctx.moveTo(node.x, node.y);
             ctx.arc(node.x, node.y, radius, 0, 2 * Math.PI)
             ctx.fill()
@@ -117,66 +111,6 @@ document.addEventListener("DOMContentLoaded", () => {
             d3.event.subject.fx = null;
             d3.event.subject.fy = null;
           }
-
-
-          // update();
-
-          // let svg = d3.select('body').append('svg')
-          //   .attr('width', 800)
-          //   .attr('height', 800);
-          //
-          //   let simulation = d3.forceSimulation()
-          //     .force("link", d3.forceLink(ladder.currGraph.links))
-          //     .force("link",d3.forceLink().id(function (d) {return d.val;}).distance(100).strength(1))
-          //     .force("charge", d3.forceManyBody())
-          //     .force("center", d3.forceCenter(800 / 2, 800 / 2));
-          //
-          //     simulation
-          //       .nodes(ladder.currGraph.graph)
-          //       .on("tick", ticked);
-          //
-          //   simulation.force("link")
-          //       .links(ladder.currGraph.links);
-          //
-          //     simulation.nodes()
-          //
-          //
-          //     function ticked() {
-          //         context.clearRect(0, 0, width, height);
-          //
-          //         context.beginPath();
-          //         graph.links.forEach(drawLink);
-          //         context.strokeStyle = "#aaa";
-          //         context.stroke();
-          //
-          //         context.beginPath();
-          //         graph.nodes.forEach(drawNode);
-          //         context.fill();
-          //         context.strokeStyle = "#fff";
-          //         context.stroke();
-          //       }
-                        // .nodes(d3.values(ladder.currGraph.graph))
-                        // .links(ladder.currGraph.links)
-                        // .gravity(.05)
-                        // .distance(100)
-                        // .charge(-100)
-                        // .start()
-                        // console.log(simulation);
-
-            // let link = svg.selectAll('.link')
-            //             .data(ladder.currGraph.links)
-            //             .enter()
-            //             .append('line')
-            //             .attr('class', 'link')
-            // //
-            // let node = svg.selectAll('.node')
-            //             .data(ladder.currGraph.graph)
-            //             .enter()
-            //             .append('circle')
-            //             .attr('class', 'node')
-            //             .attr('r', 800 * 0.03)
-
-          // console.log(ladder.currGraph)
 
         }
       }, 0)
